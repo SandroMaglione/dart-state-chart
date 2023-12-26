bda4f9893b4c94b93c1f966e643729b73e9ee642
SandroMaglione <lass.maglio@gmail.com>
2023-12-26 06:34:29 +0100

    MIT license

--- packages/core/lib/src/state.dart
+++ packages/core/lib/src/state.dart
@@ -1,8 +1,10 @@
+typedef StateAction<Context> = Context? Function(Context ctx)?;
+
 abstract class CState<Context> {
   const CState({this.onEntry, this.onExit});
 
-  final Context? Function(Context ctx)? onEntry;
-  final Context? Function(Context ctx)? onExit;
+  final StateAction<Context> onEntry;
+  final StateAction<Context> onExit;
 
   @override
   String toString() {
